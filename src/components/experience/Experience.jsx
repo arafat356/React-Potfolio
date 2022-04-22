53015d9f8046eafe3dd051e18f4310ea71282398
Oladele <oladele2alo@gmail.com>
2022-04-22 22:55:00 +0200

    build

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -100,6 +100,14 @@ export const Experience = () => {
             <article className="experience__details">
             <BsFillPatchCheckFill className='experience__details-icon'/>
             <div>
+            <h4>C++</h4>
+            <small className='text-light' >Intermediate</small>
+            </div>
+            
+            </article>
+            <article className="experience__details">
+            <BsFillPatchCheckFill className='experience__details-icon'/>
+            <div>
             <h4>Python</h4>
             <small className='text-light' >Intermediate</small>
             </div>
